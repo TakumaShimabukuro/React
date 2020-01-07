0ceed25858992c0a96c4860e0ad0c7b0080427dc
Takuma Shimabukuro <takuma@soujmv.com>
2020-01-07 01:05:42 -0300

    reactjs

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Main from "../template/Main"
+
+export default props => 
+    <Main icon="home" title="inicio" subtitle="Projeto de React">
+        <div className="display-4"> Bem vindo</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construçãp de um cadastro desenvolvido em React</p>
+    </Main>
